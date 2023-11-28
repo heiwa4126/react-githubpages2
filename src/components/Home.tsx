2bf9bf86d9058d5d5d564404a237b97a7f4f0c16
heiwa4126 <heiwa4126@gmail.com>
2023-11-28 13:57:34 +0900

    fix typo

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -7,7 +7,7 @@ function Home() {
     <>
       <h1>GitHub Pages 1</h1>
       <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-      <p>React の SPA を main ブランチ に push する毎に GitHub Pages としてデプロイするサンプル。</p>
+      <p>React の SPA を gh-pages ブランチ に push する毎に GitHub Pages としてデプロイするサンプル。</p>
       <p>
         参考: <a href="https://vitejs.dev/guide/static-deploy.html#github-pages">Deploying a Static Site | Vite</a>
       </p>
