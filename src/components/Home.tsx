ba12c80cc9eeb34c61be8b9b3a2bb54a56b38913
heiwa4126 <heiwa4126@gmail.com>
2024-09-27 17:43:11 +0900

    fix wrong text

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,17 +1,22 @@
 import { useState } from "react";
 
-function Home() {
-	const [count, setCount] = useState(0);
+function CounterButton({ initialCount = 0 }: { initialCount?: number }) {
+	const [count, setCount] = useState(initialCount);
+	return (
+		<button type="button" onClick={() => setCount((count) => count + 1)}>
+			count is {count}
+		</button>
+	);
+}
 
+function Home() {
 	return (
 		<>
 			<h1>GitHub Pages 1</h1>
-			<button type="button" onClick={() => setCount((count) => count + 1)}>
-				count is {count}
-			</button>
-			<p>
-				React の SPA を gh-pages ブランチ に push する毎に GitHub Pages としてデプロイするサンプル。
-			</p>
+			<p>SemVer 式の tag つけて push するごとに GitHub Pages としてデプロイするサンプル。</p>
+			<div>
+				<CounterButton /> <CounterButton initialCount={999} />
+			</div>
 			<p>
 				参考:{" "}
 				<a href="https://vitejs.dev/guide/static-deploy.html#github-pages">
