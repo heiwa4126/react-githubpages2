a7e3bab2182e19b8e54a49eab5cea6d8329ef014
heiwa4126 <heiwa4126@gmail.com>
2025-03-24 17:11:04 +0900

    update GITHUB_REPO_NAME

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { type ConfigEnv, type UserConfig, defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig(({ command }: ConfigEnv) => {
 	const cfg: UserConfig = {
+		base: process.env.GITHUB_REPO_NAME ?? "./",
 		// plugins: [
 		// 	react(),
 		// 	html({
