8dc89e785ae5c92c1f69af5ffe4830406a87c577
heiwa4126 <heiwa4126@gmail.com>
2025-05-08 11:01:22 +0900

    add 404.html special

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router";
+import { BrowserRouter } from "react-router";
 import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
-		<HashRouter>
+		<BrowserRouter basename="/react-githubpages2">
 			<App />
-		</HashRouter>
+		</BrowserRouter>
 	</React.StrictMode>,
 );
